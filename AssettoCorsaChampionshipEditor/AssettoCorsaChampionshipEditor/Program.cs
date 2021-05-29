1f0120e6e2c749c898ca373533b7fca0f44bf42d
Owen <duffyow@tcd.ie>
2021-05-29 01:01:30 +0100

    First pass, get names for all cars and tracks

---
+++ AssettoCorsaChampionshipEditor/AssettoCorsaChampionshipEditor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AssettoCorsaChampionshipEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string acDir = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\assettocorsa";
+            Console.WriteLine(acDir);
+            string[] cars = Directory.GetDirectories(acDir + "\\content\\cars").Select(Path.GetFileName).ToArray();
+            string[] tracks = Directory.GetDirectories(acDir + "\\content\\tracks").Select(Path.GetFileName).ToArray();
+            //
+            //
+            Console.WriteLine("\nCars:\n");
+            Array.ForEach(cars, Console.WriteLine);
+            Console.WriteLine("\n\nTracks:\n");
+            Array.ForEach(tracks, Console.WriteLine);
+        }
+    }
+}
